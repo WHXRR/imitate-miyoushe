906506308e4b3ad6f1509347f74fdf2634e9a200
WXHRR <1147232219@qq.com>
2023-12-06 17:53:27 +0800

    update

--- lib/common/scroll_to_top.dart
+++ lib/common/scroll_to_top.dart
@@ -6,24 +6,28 @@ class ScrollToTop extends StatefulWidget {
   const ScrollToTop({
     Key? key,
     required this.scrollController,
-    this.scrollThreshold = 300,
+    this.scrollThreshold = 800,
   }) : super(key: key);
 
   @override
   _ScrollToTopState createState() => _ScrollToTopState();
 }
 
-class _ScrollToTopState extends State<ScrollToTop> {
+class _ScrollToTopState extends State<ScrollToTop>
+    with SingleTickerProviderStateMixin {
   bool _isVisible = false;
+  late AnimationController _animationController;
 
   void _scrollListener() {
     if (widget.scrollController.offset >= widget.scrollThreshold &&
         !_isVisible) {
+      _animationController.forward();
       setState(() {
         _isVisible = true;
       });
     } else if (widget.scrollController.offset < widget.scrollThreshold &&
         _isVisible) {
+      _animationController.reverse();
       setState(() {
         _isVisible = false;
       });
@@ -42,10 +46,15 @@ class _ScrollToTopState extends State<ScrollToTop> {
   void initState() {
     super.initState();
     widget.scrollController.addListener(_scrollListener);
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
   }
 
   @override
   void dispose() {
+    _animationController.dispose();
     widget.scrollController.removeListener(_scrollListener);
     super.dispose();
   }
@@ -55,19 +64,22 @@ class _ScrollToTopState extends State<ScrollToTop> {
     return _isVisible
         ? InkWell(
             onTap: _scrollToTop,
-            child: Container(
-              width: 40,
-              height: 40,
-              padding: const EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                color: const Color(0xffffe14c),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: const Center(
-                child: Icon(
-                  Icons.arrow_upward,
-                  color: Colors.white,
-                  size: 20,
+            child: FadeTransition(
+              opacity: _animationController,
+              child: Container(
+                width: 40,
+                height: 40,
+                padding: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: const Color(0xffffe14c),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.arrow_upward,
+                    color: Colors.white,
+                    size: 20,
+                  ),
                 ),
               ),
             ),
