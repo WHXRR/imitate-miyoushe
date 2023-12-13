414d7f67419b9bd5a4bb0a2670c9532369ad146e
WXHRR <1147232219@qq.com>
2023-12-13 10:34:58 +0800

    update

--- lib/utils/game_category_data.dart
+++ lib/utils/game_category_data.dart
@@ -1,7 +1,7 @@
 List gameCategoryData = [
+  {"name": "崩坏：星穹铁道", "id": 6, "src": "images/4.jpg"},
   {"name": "原神", "id": 2, "src": "images/7.jpg"},
   {"name": "绝区零", "id": 8, "src": "images/5.png"},
-  {"name": "崩坏：星穹铁道", "id": 6, "src": "images/4.jpg"},
   {"name": "大别野", "id": 5, "src": "images/6.jpg"},
   {"name": "崩坏学园2", "id": 3, "src": "images/1.jpg"},
   {"name": "崩坏3", "id": 1, "src": "images/2.jpg"},
